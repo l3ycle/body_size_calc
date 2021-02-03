82c2dc17b7494051335ea0df16b480f2b130e53d
ben <gitlab@b.fam-reichel.de>
2021-02-03 11:58:21 +0100

    abb basic content

---
+++ calculator.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Body Size Calculator</title>
+  </head>
+  <body>
+
+    <?php
+
+      $name = $_GET["name"]
+
+      echo $name;
+
+     ?>
+
+  </body>
+</html>
